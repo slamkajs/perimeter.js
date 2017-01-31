c0967020a033f250cb0843c10ba4bb02c3f77543
Justin Slamka <jslamka5685@gmail.com>
2017-01-31 12:20:33 -0500

    Modified the Monitor detect function to allow for moving targets

--- dist/perimeter.debug.js
+++ dist/perimeter.debug.js
@@ -254,7 +254,7 @@
 
 /**
  * Monitor that observes the given element and detects mouse breaches
- * 
+ *
  * @param {Object} perimeter Perimeter instance
  * @return {Object}
  * @constructor
@@ -264,73 +264,75 @@
 
 (function (Perimeter, window) {
 
-	'use strict';
+    'use strict';
 
-	Perimeter.prototype.Monitor = function (perimeter) {
+    Perimeter.prototype.Monitor = function (perimeter) {
 
-		var monitor = this;
+        var monitor = this;
 
-		/**
-		 * Reference to the event object
-		 * 
-		 * @type {Object}
-		 */
-		this.event = null;
+        /**
+         * Reference to the event object
+         *
+         * @type {Object}
+         */
+        this.event = null;
 
-		/**
-		 * Detects a breach and when the cursor leaves the perimeter
-		 * 
-		 * @param {String} state either breach or leave
-		 */
-		this.detect = function (state) {
-			var	outline = perimeter.outline,
-				posX = this.event.clientX,
-				posY = this.event.clientY,
-				scrollPos = perimeter.getScrollPos(),
-				maxTop = parseInt((perimeter.rects.top - scrollPos.top - outline[0]), 10),
-				maxLeft = parseInt((perimeter.rects.left - scrollPos.left - outline[3]), 10);
+        /**
+         * Detects a breach and when the cursor leaves the perimeter
+         *
+         * @param {String} state either breach or leave
+         */
+        this.detect = function (state) {
+            var	outline = perimeter.outline,
+                target = perimeter.target,
+                posX = this.event.clientX,
+                posY = this.event.clientY,
+                scrollPos = perimeter.getScrollPos(),
+                maxTop = parseInt((target.offsetTop - scrollPos.top - outline[0]), 10),
+                maxLeft = parseInt((target.offsetLeft - scrollPos.left - outline[3]), 10);
 
-			switch (state) {
-			case 'breach':
-				if (
-					posY >= maxTop &&
-					posY < ((maxTop + perimeter.rects.height) + (outline[0] + outline[2])) &&
-					posX >= maxLeft &&
-					posX < ((maxLeft + perimeter.rects.width) + (outline[1] + outline[3]))
-				) {
-					perimeter.breaches.push([posX, posY]);
-					perimeter.trigger('breach');
-					perimeter.alarm = true;
-				}
-				break;
-			case 'leave':
-				if (
-					posY < maxTop ||
-					posY > (maxTop + perimeter.rects.height + (outline[0] + outline[2])) ||
-					posX < maxLeft ||
-					posX > (maxLeft + perimeter.rects.width + (outline[1] + outline[3]))
-				) {
-					perimeter.trigger('leave');
-					perimeter.alarm = false;
-				}
-				break;
-			}
-		};
+            switch (state) {
+            case 'breach':
+                if (
+                    posY >= maxTop &&
+                    posY < ((maxTop + perimeter.rects.height) + (outline[0] + outline[2])) &&
+                    posX >= maxLeft &&
+                    posX < ((maxLeft + perimeter.rects.width) + (outline[1] + outline[3]))
+                ) {
+                    perimeter.breaches.push([posX, posY]);
+                    perimeter.trigger('breach');
+                    perimeter.alarm = true;
+                }
+                break;
+            case 'leave':
+                if (
+                    posY < maxTop ||
+                    posY > (maxTop + perimeter.rects.height + (outline[0] + outline[2])) ||
+                    posX < maxLeft ||
+                    posX > (maxLeft + perimeter.rects.width + (outline[1] + outline[3]))
+                ) {
+                    perimeter.trigger('leave');
+                    perimeter.alarm = false;
+                }
+                break;
+            }
+        };
 
-		/**
-		 * Main observer that invokes the detection
-		 * 
-		 * @param {Object} e Event object
-		 */
-		this.observe = function (e) {
-			monitor.event = e || window.event;
-			perimeter.monitor.detect( perimeter.alarm ? 'leave' : 'breach' );
-		};
+        /**
+         * Main observer that invokes the detection
+         *
+         * @param {Object} e Event object
+         */
+        this.observe = function (e) {
+            monitor.event = e || window.event;
+            perimeter.monitor.detect( perimeter.alarm ? 'leave' : 'breach' );
+        };
 
-		return this.event;
-	};
+        return this.event;
+    };
 
 }(Perimeter, window));
+
 
 /**
  * Boundary constructor
